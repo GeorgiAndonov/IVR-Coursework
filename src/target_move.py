c748ac30c7938020c00b3a64c2fe0d9dcb7f39bc
mohsenkhadem1 <39303711+mohsenkhadem1@users.noreply.github.com>
2019-10-29 15:18:01 +0000

    Add files via upload

--- src/target_move.py
+++ src/target_move.py
@@ -15,6 +15,9 @@ def move():
   robot_joint1_pub = rospy.Publisher("/target/x_position_controller/command", Float64, queue_size=10)
   robot_joint2_pub = rospy.Publisher("/target/y_position_controller/command", Float64, queue_size=10)
   robot_joint3_pub = rospy.Publisher("/target/z_position_controller/command", Float64, queue_size=10)
+  robot_joint4_pub = rospy.Publisher("/target2/x2_position_controller/command", Float64, queue_size=10)
+  robot_joint5_pub = rospy.Publisher("/target2/y2_position_controller/command", Float64, queue_size=10)
+  robot_joint6_pub = rospy.Publisher("/target2/z2_position_controller/command", Float64, queue_size=10)
   t0 = rospy.get_time()
   while not rospy.is_shutdown():
     cur_time = np.array([rospy.get_time()])-t0
@@ -31,6 +34,17 @@ def move():
     robot_joint1_pub.publish(joint1)
     robot_joint2_pub.publish(joint2)
     robot_joint3_pub.publish(joint3)
+    x_d = 2* np.cos(cur_time * np.pi/5)
+    y_d = 2* np.sin(cur_time * np.pi/5)
+    joint4=Float64()
+    joint4.data=  x_d
+    joint5=Float64()
+    joint5.data=  y_d
+    joint6=Float64()
+    joint6.data= 5
+    robot_joint4_pub.publish(joint4)
+    robot_joint5_pub.publish(joint5)
+    robot_joint6_pub.publish(joint6)
     rate.sleep()
 
 
