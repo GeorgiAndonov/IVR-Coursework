Updated joint calculation function and set joint publishing to be latched. Joint calculation function still doesn't seem to return correct values.

src/jointcalc.py
@@ -15,9 +15,13 @@ class joint_angles:
     def __init__(self):
         self.joints1=np.array([])
         self.joints2=np.array([])
+        self.jointsactual = np.array([0.0,0.0,0.0])
         #self.jointsactual=np.array([])
         self.jointangles1 = rospy.Subscriber("camera1/robot/joints_pos",Float64MultiArray, self.callback1)
         self.jointangles2 = rospy.Subscriber("camera2/robot/joints_pos",Float64MultiArray, self.callback2)
+        self.angleactual1 = rospy.Subscriber('/robot/joint2_position_controller/command', Float64, self.callback3)
+        self.angleactual2 = rospy.Subscriber('/robot/joint3_position_controller/command', Float64, self.callback4)
+        self.angleactual3 = rospy.Subscriber('/robot/joint4_position_controller/command', Float64, self.callback5)
         #self.joint2 = rospy.Subscriber("robot/joint2_position_controller/command",Float64MultiArray, self.callback2)
         
     def callback1(self, data):
@@ -25,6 +29,16 @@ class joint_angles:
 
     def callback2(self, data):
         self.joints2=data.data
+
+    def callback3(self, data):
+        self.jointsactual[0] = data.data
+        
+    def callback4(self, data):
+        self.jointsactual[1] = data.data
+        
+    def callback5(self, data):
+        self.jointsactual[2] = data.data
+
 
 def jointcalc(angles1, angles2):
     l2 = angles1[1]
@@ -34,22 +48,23 @@ def jointcalc(angles1, angles2):
     alpha = angles1[2]
     beta = -1 * angles2[2]
     l34vector = np.array([r*np.cos(alpha)*np.cos(beta), r*np.sin(beta), r*np.sin(alpha)*np.cos(beta)])
-    l4 = np.arccos((np.dot(l23vector, l34vector)/(np.linalg.norm(l23vector)*np.linalg.norm(l34vector))))
     cross = np.cross(l23vector,l34vector)
-    v_norm = np.array([0,1,0])
-    if (np.dot(v_norm,cross)<0):
-        l4 = -l4
+    v_norm = np.array([1,0,0])
+    v_norm=v_norm/np.linalg.norm(v_norm)
+    l4 = np.arctan2(np.dot(cross, v_norm),(np.dot(l23vector, l34vector)))
     return np.array([l2,l3,l4])
 
 def main(args):
     rospy.init_node('join_calculation', anonymous=True)
     rate = rospy.Rate(1)
     jang = joint_angles()
-    while (len(jang.joints1)==0):
+    i = 0
+    while ((len(jang.joints1)==0) or (len(jang.joints2)==0)):
         rate.sleep()
-    while not rospy.is_shutdown():
-        print(jointcalc(jang.joints1,jang.joints2))
+    while not (rospy.is_shutdown() or (i>=20)):
+        print(jang.jointsactual - jointcalc(jang.joints1,jang.joints2))
         rate.sleep()
+        i+=1
     print("Shutting down")
 
 # run the code if the node is called